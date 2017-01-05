6903252f556ca90d8ebd37fc014484b0061ec06e
ashimdatt <datta.ashim2@gmail.com>
2017-01-05 09:19:07 -0800

    Update email_script.py

--- email_script.py
+++ email_script.py
@@ -21,7 +21,7 @@ def send_email(sender_address,sender_name,recipient_address,template_name,tag_li
     
     return mandrill_client.messages.send_template(template_name=template_name, template_content={}, message=message, async=False)
     import pg
-    conn = pg.DB(host="10.223.192.6", user="etl", passwd="s0.Much.Data", dbname="analytics")
+    conn = pg.DB(host="server", user="databaseuser", passwd="pass", dbname="datbasename")
 data = conn.query("select sum(case when gradescore='C' then 1 else 0 end) from ashim.eventcube_summary_raw_thez where enddate<=now()+interval '5 days' and enddate>=now()")
 data2= conn.query("select sum(case when gradescore='C' then 0 else 1 end) from ashim.eventcube_summary_raw_thez where enddate<=now()+interval '5 days' and enddate>=now()")
 data.getresult()
@@ -33,6 +33,6 @@ PERCENT=((df2/(df+df2))*100).round(1)
 perc = float(PERCENT.values[0])
 perc
 for i in range(0,12):
- send_email("datadawgs-reporting@doubledutch.me","The Dawgs", str(x.values[i,1]),"thez_version2",{'PERCENT':'%s%%' % perc,'USER_NAME':str(x.values[i,0])},"Thez- Events Activity Report")
+ send_email("email","The Dawgs", str(x.values[i,1]),"thez_version2",{'PERCENT':'%s%%' % perc,'USER_NAME':str(x.values[i,0])},"Thez- Events Activity Report")
 
  
