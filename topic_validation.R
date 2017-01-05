26da89eafce0ef5c9291fc00f637e94cd20303de
ashimdatt <datta.ashim2@gmail.com>
2017-01-05 09:20:52 -0800

    Update topic_validation.R

--- topic_validation.R
+++ topic_validation.R
@@ -1,5 +1,5 @@
 pw <- {
-  "s0.Much.Data"
+  "pass"
 }
 #setwd("/Users/ashimdatta/topic validation")
 getwd()
@@ -12,8 +12,8 @@ library("plyr")
 library("dplyr")
 
 drv <- dbDriver("PostgreSQL")
-con <- dbConnect(drv, dbname = "analytics",
-                 host = "10.223.192.6", port = 5432,
+con <- dbConnect(drv, dbname = "databasename",
+                 host = "server", port = 5432,
                  user = "etl", password = pw)
 
 
